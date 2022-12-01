f93e8714b155067e8ca226617da478094fe69771
ubuntu1104 <ataru8200@yahoo.co.jp>
2022-12-01 10:39:46 +0900

    Add a test scripts

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-import rospy, unittest, rostest
+import rospy, unittest, rostest, actionlib
 import rosnode
 import time
 
 class BuzzerTest(unittest.TestCase):
-	def test_node_exist(self):
-		nodes = rosnode.get_node_names()
-		self.assertIn('/buzzer',nodes,"node does not exist")
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('/buzzer',nodes, "node does not exist")
 
 if __name__ == '__main__':
-	time.sleep(3)
-	rospy.init_noe('travis_test_buzzer')
-	rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
+    time.sleep(3)
+    rospy.init_node('travis_test_buzzer')
+    rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
+
