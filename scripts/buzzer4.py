a2da2ec2381f25fdc287de7a5a653292bbe340b0
ubuntu1104 <ataru8200@yahoo.co.jp>
2025-06-26 14:58:01 +0900

    Ajout du script d'installation pour Travis

---
+++ scripts/buzzer4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#encoding: utf8
+import rospy, actionlib
+from std_msgs.msg import UInt16
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback 
+
+def write_freq(hz=0):
+    bfile = "/dev/rtbuzzer0"
+    try:
+        with open(bfile,"w") as f:
+            f.write(str(hz) + "\n")
+    except IOError:
+        rospy.logerr("can't write to " + bfile)
+
+def exec_music(goal): pass        
+
+def recv_buzzer(data):
+    write_freq(data.data)
+if __name__ == '__main__':
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    music = actionlib.SimpleActionServer('music', MusicAction, exec_music, False) 
+    music.start()                                                              
+    rospy.on_shutdown(write_freq)                                               
+    rospy.spin()
+
